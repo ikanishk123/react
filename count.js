16eeb68677b371e8550190cf48b790db1ea34d23
Kanishk Lamba <120341109+ikanishk123@users.noreply.github.com>
2024-07-09 17:28:59 +0530

    Update count.js

--- count.js
+++ count.js
@@ -28,3 +28,38 @@ function Alphabet(input) {
 const input = "KanishkKumarLamba";
 
 Alphabet(input);
+
+
+
+function Alphabet(input) {
+    const letters = input.toLowerCase().split('');
+    const frequency = {};
+
+   for( let i =0; i<=letters.length ; i++){
+    const char = letters[i];
+    
+    if (char>='a' && char <='z'){
+        if(frequency[char]){
+            frequency[char]++;
+        }
+        else {
+            frequency[char]=1;
+        }
+    }
+   }
+
+    let maxCount = 0;
+    let mostFrequent = '';
+
+    for (let char in frequency) {
+        if (frequency[char] > maxCount) {
+            maxCount = frequency[char];
+            mostFrequent = char;
+        }
+
+    }
+    console.log(mostFrequent)
+}
+const input = "KanishkKumarLamba";
+
+Alphabet(input);
