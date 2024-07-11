3fbb3bfe72f5d67025f6414d18935386b359a304
Kanishk Lamba <120341109+ikanishk123@users.noreply.github.com>
2024-07-11 10:51:10 +0530

    Add files via upload

---
+++ aa.js
@@ -0,0 +1,19 @@
+const array = [1, 4, 9, 16];
+const map1 = array.map((x) => x);
+console.log(map1);
+
+
+
+
+
+const array2 = [1, 4, 9, 16];
+array2.forEach((element) =>
+console.log(element));
+
+
+
+
+const numbers = [1, 4, 9];
+const roots = numbers.map((num) => Math.sqrt(num));
+console.log(roots)
+
---
+++ hello.js
@@ -0,0 +1,45 @@
+
+//hoisting
+
+hello()
+console.log(a);
+
+function hello() {
+    console.log('HELLLLLLLO');
+}
+var a = 7;
+console.log(a);
+
+
+
+//var,const,let
+
+var x = 1;
+{
+  console.log(x);
+  var x = 2;
+}
+console.log(x);
+
+
+{
+    let a = 99;
+    console.log(a);
+} 
+console.log(a);
+
+
+{
+  var a=1001;
+  console.log(a);
+}
+
+console.log(a);
+
+
+{
+  const a= 6
+  console.log(a);
+}
+
+console.log(a)
