dc6eeb48669fee508da9a6382fda9f3a3637b356
Kanishk Lamba <120341109+ikanishk123@users.noreply.github.com>
2024-07-18 10:52:24 +0530

    Add files via upload

--- count.js
+++ count.js
@@ -2,43 +2,10 @@ function Alphabet(input) {
     const letters = input.toLowerCase().split('');
     const frequency = {};
 
-    letters.forEach(function (char) {
-        if (char >= 'a' && char <= 'z') {
-            if (frequency[char]) {
-                frequency[char]++;
-            }
-            else {
-                frequency[char] = 1;
-            }
-        }
-    })
-
-    let maxCount = 0;
-    let mostFrequent = '';
-
-    for (let char in frequency) {
-        if (frequency[char] > maxCount) {
-            maxCount = frequency[char];
-            mostFrequent = char;
-        }
-
-    }
-    console.log(mostFrequent)
-}
-const input = "KanishkKumarLamba";
-
-Alphabet(input);
-
-
-
-function Alphabet(input) {
-    const letters = input.toLowerCase().split('');
-    const frequency = {};
-
    for( let i =0; i<=letters.length ; i++){
     const char = letters[i];
     
-    if (char>='a' && char <='z'){
+    if (char>='A' && char <='Z'){
         if(frequency[char]){
             frequency[char]++;
         }
